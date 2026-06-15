ddef38f6971a144eb97cc3ef5db0aad52dfdf92e
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2026-06-16 01:35:59 +0600

    change: dbname

--- backend/config/db.php
+++ backend/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=dpg-d8nubkj7uimc73a8ppng-a;dbname=apltech;port=5432', 
+    'dsn' => 'pgsql:host=dpg-d8nubkj7uimc73a8ppng-a;dbname=if0_42183052_apltech;port=5432', 
     'username' => 'if0_42183052',
     'password' => 'enkDIQpdbEBLUWwze2TNDvgHe7qIBDD2', 
     'charset' => 'utf8',
